0a41e94ec0b8daac8c66279f833a4f15f1b1624a
unknown <silvioyousseff@gmail.com>
2018-06-07 17:47:47 -0400

    pesquisar pessoas

--- src/app/lancamentos/lancamentos-cadastro/lancamentos-cadastro.component.ts
+++ src/app/lancamentos/lancamentos-cadastro/lancamentos-cadastro.component.ts
@@ -1,3 +1,4 @@
+import { PessoaService } from './../../pessoas/pessoa.service';
 import { Component, OnInit } from '@angular/core';
 
 import { ErrorHandlerService } from './../../core/error-handler.service';
@@ -17,19 +18,27 @@ export class LancamentosCadastroComponent implements OnInit {
 
   categorias = [];
 
-  pessoas = [
-    { label: 'João da Silva', value: 4 },
-    { label: 'Sebastião Souza', value: 9 },
-    { label: 'Maria Abadia', value: 3 },
-  ];
+  pessoas = [];
 
   constructor(
     private categoriaService: CategoriasService,
-    private errorHandlerService: ErrorHandlerService
+    private errorHandlerService: ErrorHandlerService,
+    private pessoaService: PessoaService
   ) { }
 
   ngOnInit() {
     this.listarCategorias();
+    this.listarPessoas();
+  }
+
+  listarPessoas(){
+    return this.pessoaService.listarTodas()
+      .then(pessoas => {
+        this.pessoas = pessoas.map(c => {
+          return { label: c.nome, value: c.id }
+        });
+      })
+      .catch(error => this.errorHandlerService.handle(error));
   }
 
   listarCategorias(){
