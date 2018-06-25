85d6d0d22d24be5232d4cfce03ebac1d97cfbee8
unknown <silvioyousseff@gmail.com>
2018-06-25 19:12:12 -0400

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
+
+import { PessoasCadastroComponent } from './pessoas/pessoas-cadastro/pessoas-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentosCadastroComponent } from './lancamentos/lancamentos-cadastro/lancamentos-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+
+const routes: Routes = [
+  { path: 'lancamentos', component: LancamentosPesquisaComponent},
+  { path: 'lancamentos/novo', component: LancamentosCadastroComponent},
+  { path: 'pessoas', component: PessoasPesquisaComponent},
+  { path: 'pessoas/novo', component: PessoasCadastroComponent}
+]
 
 @NgModule({
   declarations: [
@@ -17,6 +30,7 @@ import { LancamentosModule } from './lancamentos/lancamentos.module';
     LancamentosModule,
     CoreModule,
     HttpModule,
+    RouterModule.forRoot(routes),
 
     BrowserModule,
     BrowserAnimationsModule
